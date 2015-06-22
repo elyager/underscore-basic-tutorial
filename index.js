a1b199e7774b179deeece42baf7bf3fc6b858d69
Elyager <elyager@gmail.com>
2015-06-22 16:26:14 -0700

    Collections: where

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 var _ = require('underscore');
 
-var integers = [1, 27, 91, 32, 86, 12, 8, 68, 97, 72, 50];
-var integerLimit = 50;
+var clienList = [
+    {name:'Juan' , city:'Obregon', age:27},
+    {name:'Peter', city:'Obregon', age:31},
+    {name:'Ana'  , city:'Barcelona', age:28},
+    {name:'Oscar', city:'Obregon', age:24},
+    {name:'Dani' , city:'Bilbao', age:43},
+    {name:'Pedro', city:'Valencia', age:25},
+    {name:'Pablo', city:'Sevilla' , age:27},
+    {name:'Marta', city:'Sevilla' , age:32}
+];
 
-//functional style
-var topIntegers = _.filter(integers, function (item){ 
-    return item > integerLimit;
-});
-// ##object oriented
-var bottomIntegers = _(integers).filter(function (item){ 
-    return item < integerLimit;
-});
-	
-console.log(topIntegers);
-console.log(bottomIntegers);
+var ObregonClients = _.where(clienList, {city: 'Obregon'});
+console.log(ObregonClients);
+/*
+[ {name:'Juan' , city:'Obregon', age:27},
+  {name:'Peter', city:'Obregon', age:31},
+  {name:'Oscar', city:'Obregon', age:24},
+];
+*/

d087248efaa65c684f54ef57e2c605875eb992b9
Elyager <elyager@gmail.com>
2015-06-22 17:03:00 -0700

    Arrays: range

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
 var _ = require('underscore');
+var numbers = []
 
-var clienList = [
-    {name:'Juan' , city:'Obregon', age:27},
-    {name:'Peter', city:'Obregon', age:31},
-    {name:'Ana'  , city:'Barcelona', age:28},
-    {name:'Oscar', city:'Obregon', age:24},
-    {name:'Dani' , city:'Bilbao', age:43},
-    {name:'Pedro', city:'Valencia', age:25},
-    {name:'Pablo', city:'Sevilla' , age:27},
-    {name:'Marta', city:'Sevilla' , age:32}
-];
+numbers[0] = _.range(10);
+numbers[1] = _.range(0, 10);
+numbers[2] = _.range(1, 11);
+numbers[3] = _.range(1, 11, 2);
 
-var ObregonClients = _.where(clienList, {city: 'Obregon'});
-console.log(ObregonClients);
-
-ObregonClients = _(ObregonClients).sortBy(function(client){
-	return client.age;
-});
-
-console.log(ObregonClients);
-/*
-[ {name:'Juan' , city:'Obregon', age:27},
-  {name:'Peter', city:'Obregon', age:31},
-  {name:'Oscar', city:'Obregon', age:24},
-];
-*/
+console.log(numbers);
